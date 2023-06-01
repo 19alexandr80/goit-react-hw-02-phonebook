c0da522a66ec54c06d7c1ff4b83c20238bca3466
19alexandr80 <usanka1980@gmail.com>
2023-06-01 16:51:43 +0300

    сщььше

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template Alexandr
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+import { FormAddUser } from 'components/formPhoneBook/FormAddUser';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addUserPhoneBook = add => {
+    add.id = nanoid();
+    // this.setState({ contacts: this.state.contacts.push(add) });
+    this.state.contacts.push(add);
+    // this.setState(prevState => {
+    //   return { contacts: this.state.contacts.push(add) };
+    // });
+  };
+
+  render() {
+    return (
+      <div>
+        <FormAddUser addUserPhoneBook={this.addUserPhoneBook} />
+      </div>
+    );
+  }
+}
---
+++ src/components/formPhoneBook/FormAddUser.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export class FormAddUser extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  inputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  formSubmit = e => {
+    e.preventDefault();
+    this.props.addUserPhoneBook({
+      name: this.state.name,
+      number: this.state.number,
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.formSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Neme Surname"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.inputChange}
+          />
+        </label>
+        <label>
+          Phone
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            placeholder="+3 80..."
+            value={this.state.number}
+            onChange={this.inputChange}
+          />
+        </label>
+        <button type="submit">Add user</button>
+      </form>
+    );
+  }
+}
