faaf9107d2201fcb50e95e3b3ec1c5ad31b8f869
19alexandr80 <usanka1980@gmail.com>
2023-06-06 17:21:48 +0300

    fix '

--- src/components/formPhoneBook/FormAddUser.jsx
+++ src/components/formPhoneBook/FormAddUser.jsx
@@ -38,7 +38,7 @@ export class FormAddUser extends React.Component {
             placeholder="Neme Surname"
             value={this.state.name}
             onChange={this.inputChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([ \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
